c5e8b507d2f3e7d5e06fefcab063af29d495176e
Jyotirmayeedebadarshini <31274436+Jyotirmayeedebadarshini@users.noreply.github.com>
2017-08-30 15:49:22 +0530

    Create problem 3.cpp

---
+++ problem 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float F,C ;
+	cout<<"enter temperature in fahrenheit:";
+	cin>>F;
+	C=(F-32)*5/9;
+	cout<<"the temperature in celsius is:"<<C;
+	return 0;
+}
