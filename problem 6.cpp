2f62da79788bea2e4e22b7b110e2dce12c735103
Jyotirmayeedebadarshini <31274436+Jyotirmayeedebadarshini@users.noreply.github.com>
2017-08-30 16:28:18 +0530

    Create problem 6.cpp

---
+++ problem 6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+ 
+int main() {
+	float a,s;
+	cout<< "enter a number which squreroot is to be calculated:";
+	cin>>a;
+	s=pow(a,0.5);
+	cout<<"squreroot of a is:"<<s;
+	return 0;
+}
