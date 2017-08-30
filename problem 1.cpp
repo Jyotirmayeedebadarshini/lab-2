df1ad80a5bb59ac3a08f1755cbbee2dd6d58792e
Jyotirmayeedebadarshini <31274436+Jyotirmayeedebadarshini@users.noreply.github.com>
2017-08-30 15:22:14 +0530

    Create problem 1.cpp

---
+++ problem 1.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+	float cm,m,km;
+	cout<<"enter distance in cm:";
+	cin>>cm;
+	m=cm/100;
+	km=cm/100000;
+	cout<<"the equivalent distance in meter is:"<<m ;
+	cout<<"the equivalent distance in kilometer is km:"<<km ;
+return 0;
+}
