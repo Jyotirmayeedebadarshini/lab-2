2cb47b8aa0da1cced9ebccf29b36388ee572718a
Jyotirmayeedebadarshini <31274436+Jyotirmayeedebadarshini@users.noreply.github.com>
2017-08-30 15:37:20 +0530

    Create problem 2.cpp

---
+++ problem 2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float F,C ;
+	cout<<"enter distance in celsius:";
+	cin>>C;
+	F=(C*9/5)+32;
+	cout<<"the temperature in fahrenheit is:"<<F;
+	return 0;
+}
