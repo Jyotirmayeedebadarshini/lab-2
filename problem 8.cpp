9ae462c41bbf46fdb19c474902c892d21c40ddec
Jyotirmayeedebadarshini <31274436+Jyotirmayeedebadarshini@users.noreply.github.com>
2017-08-30 16:45:48 +0530

    Create problem 8.cpp

---
+++ problem 8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	float b,h,A;
+	cout<<"enter the base of the triangle:";
+	cin>>b;
+	cout<<"enter the height of the triangle:";
+	cin>>h;
+	A=(b*h)/2;
+	cout<<"the area of the triangle is:"<<A;
+	return 0;
+}
