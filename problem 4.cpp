68946db2b4f35bdc2a252b30cb0ab3663224b54e
Jyotirmayeedebadarshini <31274436+Jyotirmayeedebadarshini@users.noreply.github.com>
2017-09-01 18:11:19 +0530

    Create problem 4.cpp

---
+++ problem 4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float d,w,y;
+	cout<<"enter the no of days:";
+	cin>>d;
+	w=d/7;
+	y=d/365;
+	cout<<"the no of week is:"<<w;
+	cout<<"the no of year is:"<<y;
+	return 0;
+	
+}
